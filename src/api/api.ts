1409921eb8c09c4928d1d4307d9352455dc47739
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2025-01-06 15:44:00 +0000

    [Chore] SC-162578 SC-123527 upgrade deps

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,5 @@
 import {
   IDeskproClient,
-  ProxyResponse,
-  V2ProxyRequestInit,
   proxyFetch,
 } from "@deskpro/app-sdk";
 import { ICreateTimeEntry, ITimeEntry } from "../types/timeEntry";
@@ -123,7 +121,7 @@ const installedRequest = async (
 ) => {
   const fetch = await proxyFetch(client);
 
-  const options: V2ProxyRequestInit = {
+  const options: RequestInit = {
     method,
     headers: {
       "Content-Type": "application/json",
@@ -159,5 +157,5 @@ const installedRequest = async (
   return json;
 };
 
-export const isResponseError = (response: ProxyResponse) =>
+export const isResponseError = (response: Response) =>
   response.status < 200 || response.status >= 400;
